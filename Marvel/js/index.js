ea74f79e8e4ef7377af7d79b64e802ee84942e0d
Servin <12170882@itculiacan.edu.mx>
2018-05-16 11:49:35 -0600

    Mi primer PHP

--- Marvel/js/index.js
+++ Marvel/js/index.js
@@ -5,6 +5,7 @@ var marvel= function()
 		var url="http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith=";
 		var cantidadComics=0;
 		var comics="";
+		
 		url=url+personaje;
 		$.ajax({	
 			url:url,
@@ -13,9 +14,11 @@ var marvel= function()
 			{
  				if(response.code==200)//Si es Ok
  				{
+ 					$("#nombre").attr("src",response.data.results[0].name);
  					$("#foto").attr("src",response.data.results[0].thumbnail.path+"."+response.data.results[0].thumbnail.extension);
+ 					cantidadComics=response.data.results[0].comics.returned;
  				}
-			}
+			} 
 		});
 	}
     $("#btnBuscar").on("click",Buscar);
